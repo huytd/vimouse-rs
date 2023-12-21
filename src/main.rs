29f13807ee60cacd650d3c26f5a38946ef2266d2
Huy Tran <kingbazoka@gmail.com>
2023-12-21 12:01:15 -0800

    Refactor the code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::{time, thread};
+
+use std::{time, thread, collections::HashMap};
+use lazy_static::lazy_static;
 use rdev::{Event, EventType, simulate, Key, display_size, grab, Button};
 
 const SLOW_SPEED: f64 = 5.0;
@@ -10,6 +12,31 @@ static mut MOUSE_SPEED: f64 = FAST_SPEED;
 
 static mut SCREEN_WIDTH: f64 = 0.;
 static mut SCREEN_HEIGHT: f64 = 0.;
+
+lazy_static! {
+    static ref MOVEMENT_MAP: HashMap<Key, (f64, f64)> = HashMap::from([
+        (Key::KeyH, (-1.,  0.)),
+        (Key::KeyL, ( 1.,  0.)),
+        (Key::KeyJ, ( 0.,  1.)),
+        (Key::KeyK, ( 0., -1.)),
+        (Key::KeyY, (-1., -1.)),
+        (Key::KeyU, ( 1., -1.)),
+        (Key::KeyB, (-1.,  1.)),
+        (Key::KeyN, ( 1.,  1.)),
+    ]);
+
+    static ref SCREEN_CELL_MAP: HashMap<Key, (f64, f64)> = HashMap::from([
+       (Key::KeyQ, (0., 0.)),
+       (Key::KeyW, (1., 0.)),
+       (Key::KeyE, (2., 0.)),
+       (Key::KeyA, (0., 1.)),
+       (Key::KeyS, (1., 1.)),
+       (Key::KeyD, (2., 1.)),
+       (Key::KeyZ, (0., 2.)),
+       (Key::KeyX, (1., 2.)),
+       (Key::KeyC, (2., 2.)),
+    ]);
+}
 
 fn main() {
     if let Ok((w, h)) = display_size() {
@@ -55,38 +82,13 @@ fn callback(event: Event) -> Option<Event> {
                      *  b  j  n
                      *
                      */
-                    Key::KeyH => {
-                        send(&EventType::MouseMove { x: MOUSE_POSITION.0 - MOUSE_SPEED, y: MOUSE_POSITION.1 });
-                        return None;
-                    },
-                    Key::KeyL => {
-                        send(&EventType::MouseMove { x: MOUSE_POSITION.0 + MOUSE_SPEED, y: MOUSE_POSITION.1 });
-                        return None;
-                    },
-                    Key::KeyK => {
-                        send(&EventType::MouseMove { x: MOUSE_POSITION.0, y: MOUSE_POSITION.1 - MOUSE_SPEED });
-                        return None;
-                    },
-                    Key::KeyJ => {
-                        send(&EventType::MouseMove { x: MOUSE_POSITION.0, y: MOUSE_POSITION.1 + MOUSE_SPEED });
-                        return None;
-                    },
-                    Key::KeyY => {
-                        send(&EventType::MouseMove { x: MOUSE_POSITION.0 - MOUSE_SPEED, y: MOUSE_POSITION.1 - MOUSE_SPEED });
-                        return None;
-                    },
-                    Key::KeyU => {
-                        send(&EventType::MouseMove { x: MOUSE_POSITION.0 + MOUSE_SPEED, y: MOUSE_POSITION.1 - MOUSE_SPEED });
-                        return None;
-                    },
-                    Key::KeyB => {
-                        send(&EventType::MouseMove { x: MOUSE_POSITION.0 - MOUSE_SPEED, y: MOUSE_POSITION.1 + MOUSE_SPEED });
-                        return None;
-                    },
-                    Key::KeyN => {
-                        send(&EventType::MouseMove { x: MOUSE_POSITION.0 + MOUSE_SPEED, y: MOUSE_POSITION.1 + MOUSE_SPEED });
-                        return None;
-                    },
+                    Key::KeyH | Key::KeyL | Key::KeyK | Key::KeyJ | Key::KeyY | Key::KeyU | Key::KeyB | Key::KeyN => {
+                        if let Some(direction) = MOVEMENT_MAP.get(&key) {
+                            send(&EventType::MouseMove { x: MOUSE_POSITION.0 + direction.0 * MOUSE_SPEED, y: MOUSE_POSITION.1 + direction.1 * MOUSE_SPEED });
+                            return None;
+                        }
+                        return Some(event);
+                    }
                     /* Mouse clicks:
                      * - Space: Left click
                      * - Ctrl: Right click
@@ -109,78 +111,17 @@ fn callback(event: Event) -> Option<Event> {
                      *  │  Z  │  X  │  C  │
                      *  └─────┴─────┴─────┘
                      */
-                    Key::KeyQ => {
-                        let (x, y) = (
-                            0. * SCREEN_WIDTH / 3. + SCREEN_WIDTH / 6.,
-                            0. * SCREEN_HEIGHT / 3. + SCREEN_HEIGHT / 6.
-                        );
-                        send(&EventType::MouseMove { x, y });
-                        return None;
+                    Key::KeyQ | Key::KeyW | Key::KeyE | Key::KeyA | Key::KeyS | Key::KeyD | Key::KeyZ | Key::KeyX | Key::KeyC => {
+                        if let Some((col, row)) = SCREEN_CELL_MAP.get(&key) {
+                            let (x, y) = (
+                                col * SCREEN_WIDTH / 3. + SCREEN_WIDTH / 6.,
+                                row * SCREEN_HEIGHT / 3. + SCREEN_HEIGHT / 6.
+                            );
+                            send(&EventType::MouseMove { x, y });
+                            return None;
+                        }
+                        return Some(event);
                     },
-                    Key::KeyW => {
-                        let (x, y) = (
-                            1. * SCREEN_WIDTH / 3. + SCREEN_WIDTH / 6.,
-                            0. * SCREEN_HEIGHT / 3. + SCREEN_HEIGHT / 6.
-                        );
-                        send(&EventType::MouseMove { x, y });
-                        return None;
-                    }
-                    Key::KeyE => {
-                        let (x, y) = (
-                            2. * SCREEN_WIDTH / 3. + SCREEN_WIDTH / 6.,
-                            0. * SCREEN_HEIGHT / 3. + SCREEN_HEIGHT / 6.
-                        );
-                        send(&EventType::MouseMove { x, y });
-                        return None;
-                    },
-                    Key::KeyA => {
-                        let (x, y) = (
-                            0. * SCREEN_WIDTH / 3. + SCREEN_WIDTH / 6.,
-                            1. * SCREEN_HEIGHT / 3. + SCREEN_HEIGHT / 6.
-                        );
-                        send(&EventType::MouseMove { x, y });
-                        return None;
-                    },
-                    Key::KeyS => {
-                        let (x, y) = (
-                            1. * SCREEN_WIDTH / 3. + SCREEN_WIDTH / 6.,
-                            1. * SCREEN_HEIGHT / 3. + SCREEN_HEIGHT / 6.
-                        );
-                        send(&EventType::MouseMove { x, y });
-                        return None;
-                    }
-                    Key::KeyD => {
-                        let (x, y) = (
-                            2. * SCREEN_WIDTH / 3. + SCREEN_WIDTH / 6.,
-                            1. * SCREEN_HEIGHT / 3. + SCREEN_HEIGHT / 6.
-                        );
-                        send(&EventType::MouseMove { x, y });
-                        return None;
-                    },
-                    Key::KeyZ => {
-                        let (x, y) = (
-                            0. * SCREEN_WIDTH / 3. + SCREEN_WIDTH / 6.,
-                            2. * SCREEN_HEIGHT / 3. + SCREEN_HEIGHT / 6.
-                        );
-                        send(&EventType::MouseMove { x, y });
-                        return None;
-                    },
-                    Key::KeyX => {
-                        let (x, y) = (
-                            1. * SCREEN_WIDTH / 3. + SCREEN_WIDTH / 6.,
-                            2. * SCREEN_HEIGHT / 3. + SCREEN_HEIGHT / 6.
-                        );
-                        send(&EventType::MouseMove { x, y });
-                        return None;
-                    }
-                    Key::KeyC => {
-                        let (x, y) = (
-                            2. * SCREEN_WIDTH / 3. + SCREEN_WIDTH / 6.,
-                            2. * SCREEN_HEIGHT / 3. + SCREEN_HEIGHT / 6.
-                        );
-                        send(&EventType::MouseMove { x, y });
-                        return None;
-                    }
                     /* Others:
                      * - Esc: Exit
                      * - Shift: Slow speed
@@ -221,3 +162,4 @@ fn callback(event: Event) -> Option<Event> {
         };
     }
 }
+
