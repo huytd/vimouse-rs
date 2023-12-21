986332454af99b7341cff759a3ca9a543eddfdb7
Huy Tran <kingbazoka@gmail.com>
2023-12-21 11:23:13 -0800

    Add raycast script

---
+++ scripts/vimouse.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title ViMouse
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🦀
+
+# Documentation:
+# @raycast.description Mouse control using keyboard
+# @raycast.author Huy Tran
+
+echo "entered vimouse mode"
+vimouse
+echo "done"
+
